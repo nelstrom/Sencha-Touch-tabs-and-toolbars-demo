cc1a5c4c5495c2f9374a1ff43b01047587d9b5b5
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-18 17:33:10 +0200

    Limit to 5 icons in bottom tabbar.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -12,84 +12,21 @@ ToolbarDemo.views.Viewport = Ext.extend(Ext.TabPanel, {
         Ext.apply(this, {
             items: [
 				{
-					title: "action",
-					iconCls: "action"
-				},{
-					title: "add",
-					iconCls: "add"
-				},{
-					title: "arrow_down",
-					iconCls: "arrow_down"
-				},{
-					title: "arrow_left",
-					iconCls: "arrow_left"
-				},{
-					title: "arrow_right",
-					iconCls: "arrow_right"
-				},{
-					title: "arrow_up",
-					iconCls: "arrow_up"
-				},{
-					title: "compose",
-					iconCls: "compose"
-				},{
-					title: "delete",
-					iconCls: "delete"
-				},{
-					title: "organize",
-					iconCls: "organize"
-				},{
-					title: "refresh",
-					iconCls: "refresh"
-				},{
-					title: "reply",
-					iconCls: "reply"
-				},{
-					title: "search",
-					iconCls: "search"
-				},{
-					title: "settings",
-					iconCls: "settings"
-				},{
-					title: "star",
-					iconCls: "star"
-				},{
-					title: "trash",
-					iconCls: "trash"
-				},{
-					title: "maps",
-					iconCls: "maps"
-				},{
-					title: "locate",
-					iconCls: "locate"
-				},{
 					title: "home",
 					iconCls: "home",
 					badgeText: "3"
 				},{
-					title: "bookmarks",
-					iconCls: "bookmarks"
+					title: "search",
+					iconCls: "search"
 				},{
-					title: "download",
-					iconCls: "download"
+					title: "action",
+					iconCls: "action"
 				},{
-					title: "favorites",
-					iconCls: "favorites"
-				},{
-					title: "info",
-					iconCls: "info"
+					title: "settings",
+					iconCls: "settings"
 				},{
 					title: "more",
 					iconCls: "more"
-				},{
-					title: "time",
-					iconCls: "time"
-				},{
-					title: "user",
-					iconCls: "user"
-				},{
-					title: "team",
-					iconCls: "team"
 				}
             ]
         });
