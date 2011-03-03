40ab166fbc939a901f2bc16e678d8e9f62aab069
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-03 22:56:58 +0200

    Wrap form fields in a fieldset.

--- public/app/views/settingscard.js
+++ public/app/views/settingscard.js
@@ -8,18 +8,21 @@ ToolbarDemo.views.Settingscard = Ext.extend(Ext.form.FormPanel, {
     }],
     items: [
         {
+        xtype: 'fieldset',
+        title: 'Details',
+        items: [{
             xtype: 'textfield',
             name : 'name',
             label: 'Username'
-        },
-        {
+            },{
             xtype: 'passwordfield',
             name : 'password',
             label: 'Password'
-        },
-        {
-            xtype: 'button',
-            text: 'save'
+            }],
+        },{
+        xtype:  'button',
+        text:   'save',
+        ui:     'confirm'
         }
     ]
 });
