937a1fa1e07914829207254a068ef4a960fe43c8
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-03 17:44:37 +0200

    Add a search field to searchcard.js

--- public/app/views/searchcard.js
+++ public/app/views/searchcard.js
@@ -1,4 +1,11 @@
-ToolbarDemo.views.Searchcard = Ext.extend(Ext.Panel, {
-	title: "search",
-	iconCls: "search"
+ToolbarDemo.views.Searchcard = Ext.extend(Ext.form.FormPanel, {
+    title: "search",
+    iconCls: "search",
+    items: [
+        {
+            xtype: 'searchfield',
+            name : 'query',
+            label: 'Find'
+        }
+    ]
 });
