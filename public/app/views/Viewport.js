3db32197329b2bb6c5906b6cebb765cb4f3e74a3
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-03 16:30:11 +0200

    Retab with spaces.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,33 +1,33 @@
 ToolbarDemo.views.Viewport = Ext.extend(Ext.TabPanel, {
     fullscreen: true,
-	tabBar: {
-		dock: 'bottom',
-		layout: {
-			pack: 'center'
-		}
-	},
-	dock: 'bottom',
+    tabBar: {
+        dock: 'bottom',
+        layout: {
+            pack: 'center'
+        }
+    },
+    dock: 'bottom',
     cardSwitchAnimation: 'flip',
     initComponent: function() {
         Ext.apply(this, {
             items: [
-				{
-					title: "home",
-					iconCls: "home",
-					badgeText: "3"
-				},{
-					title: "search",
-					iconCls: "search"
-				},{
-					title: "action",
-					iconCls: "action"
-				},{
-					title: "settings",
-					iconCls: "settings"
-				},{
-					title: "more",
-					iconCls: "more"
-				}
+                {
+                    title: "home",
+                    iconCls: "home",
+                    badgeText: "3"
+                },{
+                    title: "search",
+                    iconCls: "search"
+                },{
+                    title: "action",
+                    iconCls: "action"
+                },{
+                    title: "settings",
+                    iconCls: "settings"
+                },{
+                    title: "more",
+                    iconCls: "more"
+                }
             ]
         });
         ToolbarDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
