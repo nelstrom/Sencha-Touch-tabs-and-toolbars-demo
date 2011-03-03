7f49cd0646e3d55f496c5073b46e64c58c7b99da
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-03 17:30:22 +0200

    Add some mock tabs to homepage.

--- public/app/views/homecard.js
+++ public/app/views/homecard.js
@@ -1,4 +1,12 @@
-ToolbarDemo.views.Homecard = Ext.extend(Ext.Panel, {
-	title: "home",
-	iconCls: "home"
+ToolbarDemo.views.Homecard = Ext.extend(Ext.TabPanel, {
+    title: "home",
+    iconCls: "home",
+    tabBar: {
+        dock: 'top'
+    },
+    styleHtmlContent: true,
+    items: [
+        {title: 'one', html: 'one'},
+        {title: 'two', html: 'two'}
+    ]
 });
