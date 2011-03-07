9378778447b3ff4b40207a924c17f155239c8d7f
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-07 22:47:39 +0200

    Add a list view to the more card.

--- public/app/views/morecard.js
+++ public/app/views/morecard.js
@@ -1,8 +1,28 @@
+Ext.regModel('Item', { fields: ['name'] });
+
+ToolbarDemo.morestore = new Ext.data.Store({
+    model: 'Item',
+    data: [
+        { name: "Bookmarks" },
+        { name: "About" }
+    ]
+});
+
 ToolbarDemo.views.Morecard = Ext.extend(Ext.Panel, {
     title: "more",
     iconCls: "more",
+    layout: 'fit',
     dockedItems: [{
         xtype: 'toolbar',
         title: "More"
     }],
+    items: [
+        {
+            xtype: 'list',
+            id: 'morelist',
+            store: ToolbarDemo.morestore,
+            itemTpl: '<div class="wrapper">{name}</div>',
+            itemSelector: 'div.wrapper'
+        }
+    ]
 });
