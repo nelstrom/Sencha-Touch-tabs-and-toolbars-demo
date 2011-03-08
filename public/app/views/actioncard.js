979c6e279ec6be49dbcc91322f8e930437ca22ec
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-08 10:44:27 +0200

    Refactor action card so toolbars are better organized.

--- public/app/views/actioncard.js
+++ public/app/views/actioncard.js
@@ -1,39 +1,70 @@
+topbar = new Ext.Toolbar({
+    title: 'Top toolbar',
+    dock: 'top',
+    items: [
+        {
+            text: 'back',
+            ui: 'back'
+        },
+        { xtype: 'spacer' },
+        {
+            text: 'edit',
+            ui: 'action'
+        }
+    ]
+});
+
+options = new Ext.Toolbar({
+    dock: 'top',
+    items: [
+        {
+            xtype: 'segmentedbutton',
+            items: [
+                { text: 'one' },
+                { text: 'one' },
+                { text: 'one' },
+            ]
+        }
+    ]
+});
+
+
 ToolbarDemo.views.Actioncard = Ext.extend(Ext.Panel, {
     title: "action",
     iconCls: "action",
-    dockedItems: [{
-        xtype: 'toolbar',
-        title: "Don't press the red button!"
-    },{
+    dockedItems: [
+        topbar,
+        options,
+        {
         xtype: 'toolbar',
         dock: 'bottom',
         ui: 'light',
         items: [
             {
-				text: 'one',
-				ui: 'back'
-			},
-			{xtype: 'spacer'},
+                text: 'one',
+                ui: 'back'
+            },
+            {xtype: 'spacer'},
             {
-				iconMask: true,
-				iconCls: 'home',
-				ui: 'plain'
-			},
+                iconMask: true,
+                iconCls: 'home',
+                ui: 'plain'
+            },
             {
-				iconMask: true,
-				iconCls: 'search',
-				ui: 'plain'
-			},
+                iconMask: true,
+                iconCls: 'search',
+                ui: 'plain'
+            },
             {
-				iconMask: true,
-				iconCls: 'settings',
-				ui: 'plain'
-			},
-			{xtype: 'spacer'},
+                iconMask: true,
+                iconCls: 'settings',
+                ui: 'plain'
+            },
+            {xtype: 'spacer'},
             {
-				text: 'three',
-				ui: 'action'
-			},
+                text: 'three',
+                ui: 'action'
+            },
         ]
     }],
 });
