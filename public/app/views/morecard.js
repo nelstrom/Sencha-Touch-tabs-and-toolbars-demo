b26efc3122f20ef76e58381bd4b37f1f3423d269
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-08 09:09:52 +0200

    Refactor the morecard to create List, rather than use xtype: 'list'

--- public/app/views/morecard.js
+++ public/app/views/morecard.js
@@ -8,21 +8,39 @@ ToolbarDemo.morestore = new Ext.data.Store({
     ]
 });
 
+ToolbarDemo.views.morelist = new Ext.List({
+    id: 'morelist',
+    store: ToolbarDemo.morestore,
+    itemTpl: '{name}',
+    listeners: {
+        itemtap: function(view, number, el, obj) {
+            console.log('tapped');
+        },
+        scope: this
+    }
+});
+
+ToolbarDemo.views.bookmarkcard = new Ext.Panel({
+    id: 'bookmarkcard',
+    html: 'Your bookmarks here'
+});
+
+ToolbarDemo.views.aboutcard = new Ext.Panel({
+    id: 'aboutcard',
+    html: 'Made from coffee'
+});
+
 ToolbarDemo.views.Morecard = Ext.extend(Ext.Panel, {
     title: "more",
     iconCls: "more",
-    layout: 'fit',
+    layout: 'card',
     dockedItems: [{
         xtype: 'toolbar',
         title: "More"
     }],
     items: [
-        {
-            xtype: 'list',
-            id: 'morelist',
-            store: ToolbarDemo.morestore,
-            itemTpl: '<div class="wrapper">{name}</div>',
-            itemSelector: 'div.wrapper'
-        }
-    ]
+        ToolbarDemo.views.morelist,
+        ToolbarDemo.views.bookmarkcard,
+        ToolbarDemo.views.aboutcard,
+    ],
 });
