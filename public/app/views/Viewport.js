241b826ea6c04b9bf6cd75a8fd9fb1c5e9fd97d7
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-08 11:54:08 +0200

    Make the Viewport fullscreen.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,4 +1,6 @@
 ToolbarDemo.views.Viewport = Ext.extend(Ext.Panel, {
+    fullscreen: true,
+    
     initComponent: function() {
         ToolbarDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
     }
