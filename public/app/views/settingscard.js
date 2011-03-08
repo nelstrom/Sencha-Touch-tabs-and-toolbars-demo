ce717ef4d51ba7eee0a0ed334616a8538c4ea988
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-08 12:36:47 +0200

    Do not add a toolbar/title to the settings card just yet.

--- public/app/views/settingscard.js
+++ public/app/views/settingscard.js
@@ -2,10 +2,6 @@ ToolbarDemo.views.Settingscard = Ext.extend(Ext.form.FormPanel, {
     title: "settings",
     iconCls: "settings",
     scroll: "vertical",
-    dockedItems: [{
-        xtype: 'toolbar',
-        title: 'Settings'
-    }],
     items: [
         {
         xtype: 'fieldset',
