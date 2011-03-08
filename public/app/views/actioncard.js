6fc334dccf2fbd2c43274c8bd305cac812d5828c
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-08 12:57:58 +0200

    Add placeholder text to actioncard.

--- public/app/views/actioncard.js
+++ public/app/views/actioncard.js
@@ -1,4 +1,6 @@
 ToolbarDemo.views.Actioncard = Ext.extend(Ext.Panel, {
     title: "action",
-    iconCls: "action"
+    iconCls: "action",
+    styleHtmlContent: true,
+    html: "placeholder text"
 });
