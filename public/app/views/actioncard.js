9c96496b4466a71a7c39ece55b5f136f41a6cd3e
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-08 11:17:00 +0200

    Add an assortment of buttons to the actioncard.

--- public/app/views/actioncard.js
+++ public/app/views/actioncard.js
@@ -1,70 +1,90 @@
-topbar = new Ext.Toolbar({
-    title: 'Top toolbar',
-    dock: 'top',
-    items: [
-        {
-            text: 'back',
-            ui: 'back'
-        },
-        { xtype: 'spacer' },
-        {
-            text: 'edit',
-            ui: 'action'
-        }
-    ]
-});
+(function() {
 
-options = new Ext.Toolbar({
-    dock: 'top',
-    items: [
-        {
-            xtype: 'segmentedbutton',
-            items: [
-                { text: 'one' },
-                { text: 'one' },
-                { text: 'one' },
-            ]
-        }
-    ]
-});
-
-
-ToolbarDemo.views.Actioncard = Ext.extend(Ext.Panel, {
-    title: "action",
-    iconCls: "action",
-    dockedItems: [
-        topbar,
-        options,
-        {
-        xtype: 'toolbar',
-        dock: 'bottom',
-        ui: 'light',
+    topbar = new Ext.Toolbar({
+        title: 'Top toolbar',
+        dock: 'top',
         items: [
             {
-                text: 'one',
+                text: 'back',
                 ui: 'back'
             },
-            {xtype: 'spacer'},
+            { xtype: 'spacer' },
             {
-                iconMask: true,
-                iconCls: 'home',
-                ui: 'plain'
-            },
-            {
-                iconMask: true,
-                iconCls: 'search',
-                ui: 'plain'
-            },
-            {
-                iconMask: true,
-                iconCls: 'settings',
-                ui: 'plain'
-            },
-            {xtype: 'spacer'},
-            {
-                text: 'three',
+                text: 'edit',
                 ui: 'action'
-            },
+            }
         ]
-    }],
-});
+    });
+
+    options = new Ext.Toolbar({
+        dock: 'top',
+        items: [
+            {
+                xtype: 'segmentedbutton',
+                items: [
+                    { text: 'one' },
+                    { text: 'two' },
+                    { text: 'three' },
+                ]
+            }
+        ]
+    });
+
+    textButtonShapes = new Ext.Toolbar({
+        dock: 'bottom',
+        items: [
+            { text: 'back',     ui: 'back' },
+            { text: 'normal',   ui: 'normal' },
+            { text: 'small',    ui: 'small' },
+            { text: 'round',    ui: 'round' },
+            { text: 'forward',  ui: 'forward' },
+        ]
+    });
+
+    textButtonColors = new Ext.Toolbar({
+        dock: 'bottom',
+        items: [
+            { text: 'action',   ui: 'action' },
+            { text: 'normal',   ui: 'normal' },
+            { text: 'confirm',  ui: 'confirm' },
+            { text: 'decline',  ui: 'decline' },
+        ]
+    });
+
+    mixedTextButtons = new Ext.Toolbar({
+        dock: 'bottom',
+        items: [
+            { text: 'action',   ui: 'action-round' },
+            { text: 'action',   ui: 'action-small' },
+            { text: 'confirm',  ui: 'confirm-round' },
+            { text: 'confirm',  ui: 'confirm-small' },
+            { text: 'decline',  ui: 'decline-round' },
+            { text: 'decline',  ui: 'decline-small' },
+        ]
+    });
+
+    imageButtons = new Ext.Toolbar({
+        dock: 'bottom',
+        items: [
+            { iconCls: 'search',  iconMask: true },
+            { iconCls: 'search',  iconMask: true,  ui: 'action' },
+            { iconCls: 'search',  iconMask: true,  ui: 'confirm' },
+            { iconCls: 'search',  iconMask: true,  ui: 'decline' },
+            { iconCls: 'search',  iconMask: true,  ui: 'plain' },
+        ]
+    });
+
+    ToolbarDemo.views.Actioncard = Ext.extend(Ext.Panel, {
+        title: "action",
+        iconCls: "action",
+        dockedItems: [
+            topbar,
+            options,
+            textButtonShapes,
+            textButtonColors,
+            mixedTextButtons,
+            imageButtons
+        ]
+    });
+
+})();
