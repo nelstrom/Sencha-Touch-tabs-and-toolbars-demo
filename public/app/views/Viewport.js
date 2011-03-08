042194128547d3fc2c436c72531880ee150af8d6
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-08 11:52:53 +0200

    Create bottom tabs for 5 panels.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,5 +1,40 @@
-ToolbarDemo.views.Viewport = Ext.extend(Ext.Panel, {
+ToolbarDemo.views.Viewport = Ext.extend(Ext.TabPanel, {
+    fullscreen: true,
+    tabBar: {
+        dock: 'bottom',
+        layout: {
+            pack: 'center'
+        }
+    },
     initComponent: function() {
+        Ext.apply(this, {
+            defaults: {
+                html: 'placeholder text',
+                styleHtmlContent: true
+            },
+            items: [
+                {
+                    title: "Home",
+                    iconCls: "home"
+                },
+                {
+                    title: "Search",
+                    iconCls: "search"
+                },
+                {
+                    title: "Action",
+                    iconCls: "action"
+                },
+                {
+                    title: "Settings",
+                    iconCls: "settings"
+                },
+                {
+                    title: "More",
+                    iconCls: "more"
+                },
+            ]
+        });
         ToolbarDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
     }
 });
