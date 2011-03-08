0def4c109f37abce0e0a29ab0737e10cb445e574
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-08 16:28:25 +0200

    Add 2 buttons to top toolbar of actioncard.

--- public/app/views/actioncard.js
+++ public/app/views/actioncard.js
@@ -5,6 +5,28 @@ ToolbarDemo.views.Actioncard = Ext.extend(Ext.Panel, {
     html: "placeholder text",
     dockedItems: [{
         xtype: "toolbar",
-        title: "Buttons"
+        title: "Buttons",
+        items: [
+            {
+                text: 'clear',
+                handler: function() {
+                    ToolbarDemo.views.homecard.tab.setBadge("");
+                }
+            },
+            { xtype: 'spacer' },
+            {
+                text: 'ping',
+                handler: function() {
+                    var hometab = ToolbarDemo.views.homecard.tab,
+                        badgenumber = parseInt(hometab.badgeText);
+
+                    if (isNaN(badgenumber)) {
+                        ToolbarDemo.views.homecard.tab.setBadge(1);
+                    } else {
+                        ToolbarDemo.views.homecard.tab.setBadge(badgenumber+1);
+                    };
+                }
+            }
+        ]
     }]
 });
