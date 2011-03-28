aaee1b5a894cbd46ba5f7da293f54a55916c483d
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-28 10:19:17 +0200

    Don't use initComponent where it's not required.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -6,36 +6,31 @@ ToolbarDemo.views.Viewport = Ext.extend(Ext.TabPanel, {
             pack: 'center'
         }
     },
-    initComponent: function() {
-        Ext.apply(this, {
-            defaults: {
-                html: 'placeholder text',
-                styleHtmlContent: true
-            },
-            items: [
-                {
-                    title: "Home",
-                    iconCls: "home",
-                    badgeText: "1"
-                },
-                {
-                    title: "Search",
-                    iconCls: "search"
-                },
-                {
-                    title: "Action",
-                    iconCls: "action"
-                },
-                {
-                    title: "Settings",
-                    iconCls: "settings"
-                },
-                {
-                    title: "More",
-                    iconCls: "more"
-                },
-            ]
-        });
-        ToolbarDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
-    }
+    defaults: {
+        html: 'placeholder text',
+        styleHtmlContent: true
+    },
+    items: [
+        {
+            title: "Home",
+            iconCls: "home",
+            badgeText: "1"
+        },
+        {
+            title: "Search",
+            iconCls: "search"
+        },
+        {
+            title: "Action",
+            iconCls: "action"
+        },
+        {
+            title: "Settings",
+            iconCls: "settings"
+        },
+        {
+            title: "More",
+            iconCls: "more"
+        },
+    ]
 });
