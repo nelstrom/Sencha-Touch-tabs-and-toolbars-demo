d2aa1b84a6c8c8be6437727e7a18f0bfffcbb463
Drew Neil <andrew.jr.neil@gmail.com>
2011-04-04 19:16:06 +0200

    Use config objects for toolbars in actioncard.

--- public/app/views/actioncard.js
+++ public/app/views/actioncard.js
@@ -1,69 +1,76 @@
-ToolbarDemo.views.topbar = new Ext.Toolbar({
-    title: "Buttons",
-    items: [
-        {
-            text: 'clear',
-            handler: function() {
-                ToolbarDemo.views.homecard.tab.setBadge("");
+(function() {
+    var topbar = {
+        xtype: "toolbar",
+        title: "Buttons",
+        items: [
+            {
+                iconCls: 'delete',  iconMask: true, ui: "plain",
+                handler: function() {
+                    ToolbarDemo.views.homecard.tab.setBadge("");
+                }
+            },
+            { xtype: 'spacer' },
+            {
+                iconCls: 'star',  iconMask: true, ui: "plain",
+                handler: function() {
+                    var hometab = ToolbarDemo.views.homecard.tab,
+                        badgenumber = parseInt(hometab.badgeText),
+                        nextnumber = isNaN(badgenumber) ? 1 : badgenumber+1;
+                    hometab.setBadge(nextnumber);
+                }
             }
-        },
-        { xtype: 'spacer' },
-        {
-            text: 'ping',
-            handler: function() {
-                var hometab = ToolbarDemo.views.homecard.tab,
-                    badgenumber = parseInt(hometab.badgeText),
-                    nextnumber = isNaN(badgenumber) ? 1 : badgenumber+1;
-                hometab.setBadge(nextnumber);
-            }
-        }
-    ]
-});
+        ]
+    };
 
-ToolbarDemo.views.textButtonShapes = new Ext.Toolbar({
-    dock: 'bottom',
-    items: [
-        { text: 'back',     ui: 'back' },
-        { text: 'normal',   ui: 'normal' },
-        { text: 'small',    ui: 'small' },
-        { text: 'round',    ui: 'round' },
-        { text: 'forward',  ui: 'forward' },
-    ]
-});
+    var textButtonShapes = {
+        xtype: "toolbar",
+        dock: 'bottom',
+        items: [
+            { text: 'back',     ui: 'back' },
+            { text: 'normal',   ui: 'normal' },
+            { text: 'small',    ui: 'small' },
+            { text: 'round',    ui: 'round' },
+            { text: 'forward',  ui: 'forward' },
+        ]
+    };
 
-ToolbarDemo.views.textButtonColors = new Ext.Toolbar({
-    dock: 'bottom',
-    items: [
-        { text: 'normal' },
-        { text: 'action',   ui: 'action' },
-        { text: 'confirm',  ui: 'confirm' },
-        { text: 'decline',  ui: 'decline' },
-    ]
-});
+    var textButtonColors = {
+        xtype: 'toolbar',
+        dock: 'bottom',
+        items: [
+            { text: 'normal' },
+            { text: 'action',   ui: 'action' },
+            { text: 'confirm',  ui: 'confirm' },
+            { text: 'decline',  ui: 'decline' },
+        ]
+    };
 
-ToolbarDemo.views.mixedTextButtons = new Ext.Toolbar({
-    dock: 'bottom',
-    items: [
-        { text: 'action',   ui: 'action-round' },
-        { text: 'action',   ui: 'action-small' },
-        { text: 'confirm',  ui: 'confirm-round' },
-        { text: 'confirm',  ui: 'confirm-small' },
-        { text: 'decline',  ui: 'decline-round' },
-        { text: 'decline',  ui: 'decline-small' },
-    ]
-});
+    var mixedTextButtons = {
+        xtype: 'toolbar',
+        dock: 'bottom',
+        items: [
+            { text: 'action',   ui: 'action-round' },
+            { text: 'action',   ui: 'action-small' },
+            { text: 'confirm',  ui: 'confirm-round' },
+            { text: 'confirm',  ui: 'confirm-small' },
+            { text: 'decline',  ui: 'decline-round' },
+            { text: 'decline',  ui: 'decline-small' },
+        ]
+    };
 
-ToolbarDemo.views.Actioncard = Ext.extend(Ext.Panel, {
-    title: "action",
-    iconCls: "action",
-    styleHtmlContent: true,
-    html: "placeholder text",
-    dockedItems: [
-        ToolbarDemo.views.topbar,
-        ToolbarDemo.views.textButtonShapes,
-        ToolbarDemo.views.textButtonColors,
-        ToolbarDemo.views.mixedTextButtons,
-    ]
-});
+    ToolbarDemo.views.Actioncard = Ext.extend(Ext.Panel, {
+        title: "action",
+        iconCls: "action",
+        styleHtmlContent: true,
+        html: "placeholder text",
+        dockedItems: [
+            topbar,
+            textButtonShapes,
+            textButtonColors,
+            mixedTextButtons
+        ]
+    });
 
-Ext.reg('actioncard', ToolbarDemo.views.Actioncard);
+    Ext.reg('actioncard', ToolbarDemo.views.Actioncard);
+})();
+
