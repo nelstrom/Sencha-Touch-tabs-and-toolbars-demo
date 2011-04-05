f7b93b47b62c010c71038b64312e0256f65ac400
Drew Neil <andrew.jr.neil@gmail.com>
2011-04-05 10:24:24 +0200

    Remove initComponent() function from viewport class.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,7 +1,3 @@
 ToolbarDemo.views.Viewport = Ext.extend(Ext.Panel, {
-    fullscreen: true,
-    
-    initComponent: function() {
-        ToolbarDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
-    }
+    fullscreen: true
 });
